851843ef3b69fa7b81b98ef10e0acfadbe5361dd
Lucas Becker <runxel@users.noreply.github.com>
2020-03-13 03:48:38 +0100

    upload "deaspect-dng"

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ PowerShell scripts I use.
 ---
 
 [apple-crap-destroyer](apple-crap-destroyer.ps1)  
-This script is used to tidy Apple-sidecar infested USB sticks.
+This script is used to tidy Apple-sidecar (like "`.DS_Store`") infested USB sticks.
+
+[deaspect-dng](deaspect-dng.ps1)  
+If you accidentally set your camera to pre-crop your files you can revert that on a whole folder of DNGs with the help of this script.
+[Needs [exiftool](https://exiftool.org/) installed and in your PATH.]
 
 [sort-files-into-folder](sort-files-into-folder.ps1)  
 Used to segregate RAW and jpeg files in a fast manner.
---
+++ deaspect-dng.ps1
@@ -0,0 +1,37 @@
+# deaspect-dng.ps1
+# script to use the exiftool on a whole folder in case you accidentally
+# enabled pre-cropping in your (DSLR) camera
+# see also
+# 
+
+$format = "dng"  # change to cr2 or orf, if you have those RAWs instead (only those possible)
+$search = "*." + $format
+$suffix = "-da"  # suffix for renaming of the de-aspected files
+
+
+function Deaspect($Filename){
+	
+	# those are getting the true original sizes
+	$CmdWidth = exiftool -ImageWidth $Filename
+	$CmdHeight = exiftool -ImageHeight $Filename
+	
+	$ImgWidth = $CmdWidth -replace '\D+'
+	$ImgHeight = $CmdHeight -replace '\D+'
+
+	$NewFilename = $Filename.BaseName + $suffix + $Filename.Extension
+	
+	&exiftool -AspectRatio="" -AspectFrame="0 0 0 0" `
+	-DefaultCropOrigin="" -DefaultCropSize="" `
+	-CroppedImageWidth="$ImgWidth" `
+	-CroppedImageHeight="$ImgHeight" `
+	-o $NewFilename $Filename
+
+	<# note:
+	   you can also be explicit in what you set for "AspectRatio"
+	   sometimes exiftool will yell at you "can't delete Permanent tag"
+	   for me it still worked always, but your mileage may very
+	   Find out what the original ratio was and set it: e.g. "4:3"
+	#>
+}
+
+Get-ChildItem -Filter $search -Recurse | ForEach-Object { Deaspect -Filename $_ }
